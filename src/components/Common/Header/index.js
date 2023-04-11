1d616f14cb2931e37ad55b73b00561f4c6c5844e
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-04-11 22:31:22 +0530

    created app files and started working on navbar

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+function Header() {
+  return (
+    <div className='navbar'>
+
+    <h1 className='logo'>CryptoTracker <span style={{color:'var(--Orange)'}}>.</span></h1>
+    <div className='links'>
+        <a href='/'><p className='link'>Home</p></a>
+        <a href='/'><p className='link'>Compare</p></a>
+        <a href='/'><p className='link'>Watchlist</p></a>
+       
+    </div>
+    </div>
+  )
+}
+
+export default Header
