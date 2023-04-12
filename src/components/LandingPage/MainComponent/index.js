13a89890397ca4ff007194d11a2b157e784e252e
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-04-12 14:53:19 +0530

    Made iphone images animated using framer-motion

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -12,15 +12,15 @@ function MainComponent() {
 }>Track</motion.h1>
 <motion.h1 className='track-heading' initial={{opacity:0.3, x:70}} animate={{opacity:1,x:0}} transition={{duration:1}
 }>Crypto</motion.h1>
-<motion.h1 className='realtime-heading'initial={{opacity:0.3, x:-70}} animate={{opacity:1,x:0}} transition={{duration:0.5}}>Real Time.</motion.h1>
-<p className='info-text'>Track crypto thorugh a public API in real time . Visit the dashboard to do so.</p>
-<div className='btn-flex'>
+<motion.h1 className='realtime-heading'initial={{opacity:0.1, y:70}} animate={{opacity:1,y:0}} transition={{duration:0.5,delay:0.5}}>Real Time.</motion.h1>
+<motion.p className='info-text' initial={{opacity:0, y:100}} animate={{opacity:1,y:0}} transition={{duration:0.5,delay:0.5}}>Track crypto thorugh a public API in real time . Visit the dashboard to do so.</motion.p>
+<motion.div className='btn-flex' initial={{opacity:0, x:70}} animate={{opacity:1,x:0}} transition={{duration:1,delay:1.2}}>
     <Button text='Dashboard' />
     <Button text='Share' outlined={true} />
-</div>
+</motion.div>
 </div>
 <div className='phone-component'>
-<img src={iphone} alt='iphone' className='iphone'/>
+<motion.img src={iphone} alt='iphone' className='iphone'initial={{ y:-10}} animate={{y:10}} transition={{type:'smooth',repeatType:'mirror',duration:1.2,repeat:Infinity}}/>
 <img src={gradient}  alt='gradient'className='gradient'/>
 </div>
 
