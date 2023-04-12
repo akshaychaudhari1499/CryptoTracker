061894ce254956e21092b5ac42491383d4854fa3
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-04-12 14:37:49 +0530

    Added framer motion to the headings

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -3,13 +3,16 @@ import './styles.css'
 import Button from '../../Common/Button'
 import iphone from '../../../assets/iphone.png'
 import gradient from '../../../assets/gradient.png'
+import {motion } from 'framer-motion'
 function MainComponent() {
   return (
     <div className='flex-info'>
 <div className='left-component'>
-<h1 className='track-heading'>Track</h1>
-<h1 className='track-heading'>Crypto</h1>
-<h1 className='realtime-heading'>Real Time.</h1>
+<motion.h1 className='track-heading' initial={{opacity:0.3, x:-70}} animate={{opacity:1,x:0}} transition={{duration:0.5}
+}>Track</motion.h1>
+<motion.h1 className='track-heading' initial={{opacity:0.3, x:70}} animate={{opacity:1,x:0}} transition={{duration:1}
+}>Crypto</motion.h1>
+<motion.h1 className='realtime-heading'initial={{opacity:0.3, x:-70}} animate={{opacity:1,x:0}} transition={{duration:0.5}}>Real Time.</motion.h1>
 <p className='info-text'>Track crypto thorugh a public API in real time . Visit the dashboard to do so.</p>
 <div className='btn-flex'>
     <Button text='Dashboard' />
@@ -17,8 +20,8 @@ function MainComponent() {
 </div>
 </div>
 <div className='phone-component'>
-<img src={iphone} className='iphone'/>
-<img src={gradient} className='gradient'/>
+<img src={iphone} alt='iphone' className='iphone'/>
+<img src={gradient}  alt='gradient'className='gradient'/>
 </div>
 
     </div>
