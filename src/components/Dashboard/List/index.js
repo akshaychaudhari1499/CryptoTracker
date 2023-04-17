4e75b796995f21235a6526153cd28e7eae51c7fc
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-04-17 23:49:15 +0530

    made  changes for mobile view of list component

--- src/components/Dashboard/List/index.js
+++ src/components/Dashboard/List/index.js
@@ -1,56 +1,64 @@
 import React from "react";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import Tooltip from '@mui/material/Tooltip';
 import "./styles.css";
 function List({ coin }) {
   return (
     <tr className="list-row">
       <td>
-        <img className="coin-logo" alt={coin.name} src={coin.image}></img>
+        <img className="coin-logo font" alt={coin.name} src={coin.image}></img>
       </td>
       <td >
-        <p className="coin-symbol">{coin.symbol}-USD</p>
-        <p className="coin-name">{coin.name}</p>
+        <p className="coin-symbol hidden">{coin.symbol}-USD</p>
+        <p className="coin-name font">{coin.name}</p>
       </td>
 
       {coin.price_change_percentage_24h > 0 ? (
         <>
-          <td className="chip-flex">
-            <p className="price-chip">
+        <Tooltip title='current_price'>
+          <td className="chip-flex font">
+            <p className="price-chip font">
               +{coin.price_change_percentage_24h.toFixed(2)}%
             </p>
           </td>
+          </Tooltip>
+          
           <td>
-            <p className="icon-chip">
+            <p className="icon-chip hidden ">
               <TrendingUpIcon />
             </p>
           </td>
         </>
       ) : (
-        <>
+        <><Tooltip title='current_price'>
           <td className="chip-flex">
-            <p className="price-chip red">
+            <p className="price-chip red font">
               {coin.price_change_percentage_24h.toFixed(2)}%
             </p>
           </td>
+          </Tooltip>
           <td>
-            <p className="icon-chip">
+            <p className="icon-chip red hidden">
               <TrendingDownIcon />
             </p>
           </td>
         </>
       )}
-      <td>
-        <h3 className={coin.price_change_percentage_24h > 0 ? "up" : "down"}>
+      <Tooltip title='current_price'><td >
+        <h3 className={coin.price_change_percentage_24h > 0 ? "up center right font" : "down center right font"}>
           ${coin.current_price.toLocaleString()}
         </h3>
+      </td> 
+      </Tooltip>
+      
+     <Tooltip title='volume'>
+      <td className="price-container ">
+        <h3 className="volume hidden">{coin.total_volume.toLocaleString()}</h3>
       </td>
-     
-      <td className="price-container">
-        <h3 className="volume">{coin.total_volume.toLocaleString()}</h3>
-      </td>
+      </Tooltip>
       <td>
-        <h3 className="cap">{coin.market_cap.toLocaleString()}</h3>
+        <h3 className="cap hidden">{coin.market_cap.toLocaleString()}</h3>
       </td>
     </tr>
   );
