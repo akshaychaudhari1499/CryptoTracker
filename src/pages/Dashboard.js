556039ae0623835f8fa1bf07c82397d6734f20ae
Akshay Chaudhari <akshaychaudhari1499@gmail.com>
2023-04-18 11:11:13 +0530

    Made a filter function for the searchbar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,6 +10,7 @@ function onSearchChange(e){
   console.log(e.target.value);
   setSearch(e.target.value)
 }
+const filteredCoins=coins.filter((coin)=>coin.name.toLowerCase().includes(search))
 useEffect(()=>{
  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en').then(response=>setCoins(response.data)).catch(err=>console.error());
 },[])
@@ -20,7 +21,7 @@ console.log(coins)
   return (
     <div><Header/> 
     <Search search={search} onSearchChange={onSearchChange}/>
-    <TabsComponent coins={coins}/></div>
+    <TabsComponent coins={filteredCoins}/></div>
    
   )
 }
